fce8584dac385cabb5f74bf2a2e754e58ac3ee96
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-11-12 20:24:36 -0800

    output file name

--- command/new.ts
+++ command/new.ts
@@ -3,8 +3,7 @@ import { format } from "./new-deps.ts";
 
 export const new_ = async (ctx: Context) => {
   const timestamp = format(new Date(), "yyyy-MM-ddTHH:mm:ss");
-  await Deno.writeTextFile(
-    `./${ctx.denoteProject}-${timestamp}.md`,
-    `# ${timestamp}\n\n## New Note`
-  );
+  const filename = `./${ctx.denoteProject}-${timestamp}.md`;
+  await Deno.writeTextFile(filename, `# ${timestamp}\n\n## New Note`);
+  console.log(filename);
 };
