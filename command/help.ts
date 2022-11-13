6c7570e05c7c48be224683e14855e32e5f13985c
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-11-12 20:35:07 -0800

    update help

--- command/help.ts
+++ command/help.ts
@@ -1,3 +1,20 @@
 export const help = () => {
-  console.log("Usage: denote <command> [options]");
+  console.log(helpSring);
 };
+
+const helpSring = `Usage: denote <command> [options]
+
+command             description
+-------------------------------------------------------------------------------
+new                 Create a new note
+compile             Compile notes
+search <pattern>    Search notes
+show                Show notes
+open                Open notes in editor
+
+option              type                    description         default
+--------------------------------------------------------------------------------
+-p, --project       string                  Project name        denote
+--denoteHome        string                  Denote home         \${HOME}/.denote
+--compileMode       append OR prepend       Compile mode        prepend
+`;
